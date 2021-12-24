36999062fc27096283bedda61b68b62188328997
Ferronn <ferronn@ferronn.dev>
2021-12-24 11:03:43 -0600

    fixup rsyncs

--- query.sh
+++ query.sh
@@ -1,5 +1,5 @@
 export HOME=/root
-gsutil rsync gs://wowless.dev/luadbd "$HOME"/.cache/luadbd
+gsutil -m rsync -u gs://wowless.dev/luadbd "$HOME"/.cache/luadbd
 /usr/local/bin/wowcig -z -v -c "" -d all -p "$1"
-gsutil cp -r extracts gs://wowless.dev/extracts/
-gsutil rsync gs://wowless.dev/luadbd "$HOME"/.cache/luadbd
+gsutil -m rsync -u extracts gs://wowless.dev/extracts
+gsutil -m rsync -u "$HOME"/.cache/luadbd gs://wowless.dev/luadbd
